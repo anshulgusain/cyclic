561a7c18cbc26198854237013f4dea99c5d85a0b
Anshul Gusain <114215074+anshulgusain@users.noreply.github.com>
2024-09-07 12:40:58 +0530

    Delete Backend/backend directory

--- Backend/backend/connection/connect.js
+++
@@ -1,8 +0,0 @@
-const mongoose=require("mongoose")
-require('dotenv').config()
-// console.log(process.env.PASSWORD)
-const connection=mongoose.connect(`mongodb+srv://anshulgusain99:${process.env.PASSWORD}@cluster0.vwiwavz.mongodb.net/dashify`)
-
-module.exports={
-    connection
-}
